43d1b56a3e0dab1225f72d2089dbb4a9900a7258
thiemom <thiemom@hotmail.com>
2014-10-25 15:26:16 +0200

    shiny app working v1.0

--- server.R
+++ server.R
@@ -1,14 +1,13 @@
 # load required packages
 require(shiny)
 require(ggplot2)
-require(rpart)
-require(rattle)
-require(rpart.plot)
-# require(RColorBrewer)
+require(caret)
+require(pROC)      
 
 # read the data
-load('titanictrain.Rdata')
-load('titanictest.Rdata')
+load('titanic-train.RData')
+load('titanic-test.RData')
+load('titanic-varimp.RData')
 
 # join data
 combi <- rbind(train, test)
@@ -16,6 +15,10 @@ combi <- rbind(train, test)
 # server logic required to run ui.R
 shinyServer(function(input, output) {
 
+  # dataset selection
+  # train, test are loaded from data and combi is created by binding train and test
+  # depending on the user selection one of the three sets is assigned to the variable dataset
+  # this is reactive so it will update all the output of the Shiny App
   dataset <- reactive({
     switch(input$dataset,
            "train" = train,
@@ -23,88 +26,148 @@ shinyServer(function(input, output) {
            "combined" = combi)
   })
 
-  output$summary_of_x <- renderPrint({
-    v <- input$x
-    data <- dataset()
-    summary(data[v])
-  })
-
-  output$summary_of_y <- renderPrint({
-    v <- input$y
-    data <- dataset()
-    summary(data[v])
-  })
-
-  output$summary_of_color <- renderPrint({
-    if (input$color != 'None') {
-      v <- input$color
-      data <- dataset()
-      summary(data[v])
+  # filter data based on selection in the data summary tab
+  output$table <- renderDataTable({
+    
+    d <- dataset() # init data 
+    
+    if (input$sex != "All"){
+      d <- d[d$Sex == input$sex,]
     }
+    if (input$title != "All"){
+      d <- d[d$Title == input$title,]
+    }
+    if (input$embarked != "All"){
+      d <- d[d$Embarked == input$embarked,]
+    }
+    if (input$fate != "All"){
+      d <- d[d$Fate == input$fate,]
+    }
+    d
   })
-
+  
+  # scatter plot for data exploration
   output$scatter <- renderPlot({
-    data <- dataset()
-    p <- ggplot(data,
+    
+    d <- dataset() # init data
+    
+    # create scatter plot (plot x versus y)
+    p <- ggplot(d,
                 aes_string(x=input$x,
                            y=input$y)) + geom_point()
 
+    # color the data points
     if (input$color != 'None') {
       p <- p + aes_string(color=input$color)
     }
-
+    # this adds the default smoother to the plot
+    # the method of smoothing depends on the number of data points
     if (input$smooth) {
       p <- p + geom_smooth()
     }
-
-    print(p)
-
-  })
-
-  output$box <- renderPlot({
-
-    cat(input$x, input$y, input$color, '\n')
-
-    p <- ggplot(data = dataset(),
-                mapping = aes(factor(input$x),
-                              input$y))
-
-    if (input$color != 'None') {
-      p <- p + geom_boxplot(mapping = aes_string(
-        fill = factor(input$color)))
+    # this adds jitter to the plot
+    # jitter introduces random scatter to avoid overplotting
+    if (input$jitter) {
+      p <- p + geom_jitter()
     }
-    else {
-      p <- p + geom_boxplot()
+    print(p)
+
+  })
+  
+  # bar plot for variable importance
+  output$variableimportance <- renderPlot({
+    
+    # the variable importance pre-computed and not reactive here to speed up things for this demo project 
+    # it would be easy to change later by changing the init here
+    d <-  data.frame(svImp) # init data to a dataframe 
+    
+    # get reactive slider value for minimum relative variable importance score
+    minimp <- input$sliderminvimp
+    
+    # add feature names
+    d$features <- names(svImp)
+    
+    # create new feature to track if a variable is selected or not
+    # this will be reactive and linked to the slider input
+    d$selected <- 0 # init to 0 --> not selected
+    d$selected[d$svImp >= minimp] = 1 # set features with a relative importance score larger or equal to slider to 1
+    d$selected <- factor(d$selected) # make factor for plotting
+
+    # safety check if anything is selected at all 
+    # because I use the min/max values from the dataset in ui.R this cannot happen
+    # however, I'd like this code independent of the exact ui.R implementation
+    if (sum(d$selected != 0)) {
+      # create a bar chart which is by default vertical
+      # identity means not to use bins but the value for the bar height
+      # pretty important since each value is probably unique and the bins would be all equal
+      # and finally flip it to make it horizontal
+      p <- ggplot(data=d, aes(x=features, y=svImp, fill=selected)) + geom_bar(stat="identity") + coord_flip()
     }
-
+    
     print(p)
-
+    
   })
-
-  output$tree <- renderPlot({
-
-    # reactive update of dataset
-    data <- dataset()
-
-    # build a tree of the features
-    fit <- rpart(Survived ~ Pclass + Sex + Age +
-                   Fare + Embarked + Title + FamilySize +
-                   Child, data=data, method="class")
-
-    complexity <- input$complexity
-
-    fit <- prune(fit, cp = complexity)
-
-    cat(data$names)
-    cat(fit$variable.importance)
-
-    p <- fancyRpartPlot(fit)
-
+  
+  # build logistic regression model with the selected variables
+  # then use this model to show the ROC (Receiver Operating Characteristic) plot 
+  output$roc <- renderPlot({
+  
+    # accessing input$goButton here makes this reactive code depend on it
+    # when the input$goButton changes, the code will re-execute
+    # it is better to do this here because it can take quite some time to calculate this block
+    # note that the code is executed on initialization 
+    input$goButton
+    
+    isolate({
+      
+      # get reactive slider value for minimum relative variable importance score
+      minimp <- input$sliderminvimp
+      
+      # select features for variable selection
+      Keep <- c("Fate", names(svImp[svImp>minimp]))
+      
+      # init training data
+      d <- train[Keep]
+    
+      # use fixed random seed for repeatability
+      set.seed(2014)
+      
+      # split data into train batch and test batch using caret functionality
+      train.rows <- createDataPartition(d$Fate, p=0.8, list = FALSE)
+      train.batch <- d[train.rows, ] # training
+      test.batch <- d[-train.rows, ] # test
+      
+      # define control parameters to handle optional arguments for train function
+      cv.ctrl <- trainControl(method = "repeatedcv", repeats = 3, summaryFunction = twoClassSummary, classProbs = TRUE)
+      
+      # train the logistic regression model
+      glm <- train(Fate ~ ., data = train.batch,
+                   method = "glm", metric = "ROC", trControl = cv.ctrl)
+      
+      # use model to generate predictions using the test data which was not used to train the model
+      glm.pred <- predict(glm, test.batch)
+      confusionMatrix(glm.pred, test.batch$Fate)
+      glm.probs <- predict(glm, test.batch, type = "prob")
+      
+      # calculate ROC
+      glm.ROC <- roc(response = test.batch$Fate,
+                     predictor = glm.probs$Survived,
+                     levels = levels(test.batch$Fate))
+      
+      # create reactive text output with the compute area under the curve
+      output$auc <- renderText({ 
+        paste("Area under the curve:", auc(glm.ROC))
+      })
+      
+      # plot ROC using the stair steps (S) plot option
+      p <- plot(glm.ROC, type="S", col='blue', lwd=5)
+      
+    }) # end isolate
+    
     print(p)
-
+    
   })
-
-
+  
 
 
 })
--- ui.R
+++ ui.R
@@ -1,50 +1,200 @@
 # load required packages
 require(shiny)
 require(ggplot2)
-require(rpart)
+require(caret)
+require(pROC)      
 
 # read the data
-load('titanictrain.Rdata')
-load('titanictest.Rdata')
+load('titanic-train.Rdata')
+load('titanic-test.Rdata')
+load('titanic-varimp.RData')
 
 # join data
 dataset <- rbind(train, test)
 
+# shinyUI(pageWithSidebar(
 shinyUI(pageWithSidebar(
-
-  headerPanel("Titanic: machine learning from disaster"),
-
+  
+  # show main title
+  headerPanel("Titanic: machine learning from disaster ... a Shiny App"),
+  
+  # define content of sidebar panel
   sidebarPanel(
+  
+    # let's add some text
+    # I need to remember to end each code line with a comma, except the last line
+    
+    # add introduction as header level 2
+    h2("Introduction"),
+  
+    # describe the Titanic data 
+    h3("The Titanic"),
+    
+    tags$p(
+      "The Titanic was a British passenger liner that sank in the North Atlantic Ocean in the early morning of 15 April 1912 
+      after colliding with an iceberg during her maiden voyage from Southampton, UK to New York City, US. 
+      The sinking of Titanic caused the deaths of more than 1,500 people in one of the deadliest peacetime maritime disasters 
+      in modern history. Source:",
+      tags$a(href="http://en.wikipedia.org/wiki/RMS_Titanic", "Wikipedia"),
+      # add an image of the titanic taken from wikipedia.com
+      # the image must be in a sub-directory called "www" which must be in the same directory as ui.R
+      img(src="600px-RMS_Titanic_3.jpg")
+    ), 
+    
+    # add a paragraph to talk about the Kaggle competion
+    tags$p(
+      "Titanic is also the theme for a machine learning competition on",
+      tags$a(href="https://www.kaggle.com", "kaggle."),
+      "The Titanic competition is a so called knowledge competition designed to learn as well 
+      as test your knowledge by competing against the community."
+    ),
+    
+    tags$p(
+      "I have used the Titanic competion as theme for my",
+      tags$a(href="http://shiny.rstudio.com", "Shiny App"),
+      "that I created for the",
+      tags$a(href="https://www.coursera.org", "Coursera"),
+      "course on Developing Data Products by the",
+      tags$a(href="https://www.coursera.org/jhu", "John Hopkins University.")
+    ),
+    
+    h3("About me"),
+    tags$p(
+      "My name is Thiemo. I am an engineer working in the Energy industry. 
+      One of my personal interests is Machine Learning. Ever since I discovered",
+      tags$a(href="https://www.coursera.org", "Coursera"),
+      "I have been a great fan of it. Just wish I had more time to devote to it!"
+    ),
+    
+    # now add the dataset selector
+    h3("Select the dataset"),
+    selectInput("dataset", "Dataset:",
+                choices = c("train", "test", "combined"),
+                selected = "train"),
+    
+    tags$p(
+      "For more information on the data and the meaning of the variables have a look at the",
+      tags$a(href="https://www.kaggle.com/c/titanic-gettingStarted", "Titanic competion on Kaggle")
+    ),
+    
+    tags$p(
+      "More details about the modeling can be found on my",
+      tags$a(href="", "Titanic github"),
+      "page."  
+    )
+    
+  , width = 4), # this end the sidebar panel
 
-    selectInput("dataset", "Choose a dataset:",
-                choices = c("train", "test", "combined")),
-
-    selectInput('x', 'X', names(dataset), 'Age'),
-
-    selectInput('y', 'Y', names(dataset), 'Sex'),
-
-    selectInput('color', 'Color',
-                c('None', names(dataset)), 'Survived'),
-
-    checkboxInput('smooth', 'Smooth')
-
-  ),
-
-  # show a tabset with summary and plot views
+  # define content of the main panel
   mainPanel(
+    
+    # create tabs in main panel
     tabsetPanel(
-      tabPanel("Summary",
-               verbatimTextOutput("summary_of_x"),
-               verbatimTextOutput("summary_of_y"),
-               verbatimTextOutput("summary_of_color")),
+      # first tab will show a data table 
+      tabPanel("Data",
+               # first create a fluid row for input selectors
+               # the number after column controls the width of the selector
+               fluidRow(
+                 column(3, 
+                        selectInput("sex", 
+                                    "Sex:", 
+                                    c("All", 
+                                      unique(as.character(dataset$Sex))))
+                 ),
+                 column(3, 
+                        selectInput("title", 
+                                    "Honorific:", 
+                                    c("All", 
+                                      unique(as.character(dataset$Title))))
+                 ),
+                 column(3, 
+                        selectInput("embarked", 
+                                    "Embarked:", 
+                                    c("All", 
+                                      unique(as.character(dataset$Embarked))))
+                 ),
+                 column(3, 
+                        selectInput("fate", 
+                                    "Fate:", 
+                                    c("All", 
+                                      unique(as.character(dataset$Fate))))
+                 ) 
+               ),
+               # now create a new row for the table
+               fluidRow(
+                 dataTableOutput(outputId="table")
+               )    
+      ),
+      
+      tabPanel("Explore",
+               
+               # first create a fluid row for variable selectors
+               fluidRow(
+                 column(4,  
+                        selectInput("x",
+                                    "X variable:",
+                                    unique(as.character(names(dataset))),
+                                    'Age')
+                 ),
+                 column(4,  
+                        selectInput("y",
+                                    "Y variable:",
+                                    unique(as.character(names(dataset))),
+                                    'Sex')
+                 ),
+                 column(4,  
+                        selectInput("color",
+                                    "Color by:",
+                                    unique(as.character(names(dataset))),
+                                    'Fate')
+                 )
+               ),
+              
+               # add a row for plot options
+               fluidRow(
+                 column(4,  
+                        checkboxInput('smooth', 'Add smoother', value = FALSE)
+                 ),
+                 column(4,  
+                        checkboxInput('jitter', 'Add jitter', value = TRUE)
+                 )
+               ),
+               
+               # and finally add a row for the plot
+               fluidRow(
+                 plotOutput("scatter")
+               )
+                
+      ), # ends tab plot
+      tabPanel("Model",
+               
+               # create row for variable importance selection and go button
+               fluidRow(
+                 column(8,
+                        sliderInput("sliderminvimp", label = "Minimum Variable Importance Score",
+                                    min = 0, max = max(svImp), value = mean(svImp))
+                 ),
+                 column(4,
+                        actionButton("goButton", "Update model")
+                 )
+               ),
+               
+               # add row for the variable importance plot
+               fluidRow(
+                h5("Relative Variable Importance"),
+                plotOutput("variableimportance")
+               ),
+               
+               # finally add row for the ROC plot
+               fluidRow(
+                 h5("Receiver Operating Characteristic of the Logistic Regression Model"),
+                 # add the plot
+                 plotOutput("roc"),
+                 # utput area under the curve (auc)
+                 textOutput("auc")
+               )
 
-      tabPanel("Box plot",
-               h2("Trees"),
-               sliderInput("decimal", "Complexity:",
-                           min=0.1, max=1.0,
-                           value=0.5, step = 0.1),
-               plotOutput("tree")),
-      tabPanel("Scatter plot", plotOutput("scatter"))
+      ) # ends tab model
     )
   )
 
